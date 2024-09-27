9a145f28c21713ecd0b6e92ae2ea0950bc48d17c
Nikolai <nikolai.pulman@npu.ee>
2024-09-27 13:15:17 +0300

    Update NPU-EstonianIDCard.psm1

--- NPU-EstonianIDCard/NPU-EstonianIDCard.psm1
+++ NPU-EstonianIDCard/NPU-EstonianIDCard.psm1
@@ -1,69 +1,67 @@
 #
-# 
-#
 # By: Nikolai Pulman
 #
-# 
-# 
+# On: 3/20/2024
+#
+ 
 
-#Set some global parameters
+# Set some global parameters
 $global:EstIDLdapURL = "esteid.ldap.sk.ee"
 $global:EstIDLdapPort = 636
-$global:EstIDLdapDN = "ou=Authentication,o=Identity card of Estonian citizen,dc=ESTEID,c=EE"
+$global:EstIDLdapDN = "dc=ESTEID,c=EE"
+$global:NationalIDDN = "*ou=Authentication,o=Identity card of Estonian citizen,dc=ESTEID,c=EE"
+$global:DigitalIDDN = "*ou=Authentication,o=Digital identity card,dc=ESTEID,c=EE"
 
+#
+#
+#   Version 1.1.0 rewrite
+#   Add digital id mapping(Blue cards)
+#
+#
 
-function Get-EstonianIDMapping {
-    param(
-        [Parameter(Mandatory = $true)] [String] $EstonianID
-    ) 
-    #Define LDAP
+#
+#   Get user mapping from sk.ee as array
+#
+
+function Get-IDUserMapping {
+    param([Parameter(Mandatory = $true)] [String] $EstonianID) 
+    # Define LDAP
     $ldapdn = 'LDAP://' + $global:EstIDLdapURL + ":" + $EstIDLdapPort + "/" + $EstIDLdapDN
     $auth = [System.DirectoryServices.AuthenticationTypes]::Anonymous
     $ldap = New-Object System.DirectoryServices.DirectoryEntry($ldapdn, $null, $null, $auth) 
 
-    #LDAP Searcher
+    # LDAP Searcher
     $ds = New-Object System.DirectoryServices.DirectorySearcher($ldap)
     $IDCodeFilter = "(serialNumber=PNOEE-" + $EstonianID + ")"
-
     $ds.Filter = $IDCodeFilter
     [void]$ds.PropertiesToLoad.Add("usercertificate;binary")
 
-    #Results
+    # Results
     $SearchResults = $ds.FindAll()
 
-    #Terminate if National Certificate was not found:
-    if ($SearchResults.Count -Eq 0) {
-        Write-Error 'User Certificate not found!' -ErrorAction Stop
+    # Mapping results
+    $MappingResults = New-Object System.Collections.Generic.List[System.Object]
+
+
+    # Search certificates
+    foreach ($result in $SearchResults) {
+        # National ID
+        if ($result.Path -like $global:NationalIDDN ) {
+            [byte[]]$ByteCertificate = $Result.Properties.'usercertificate;binary' | out-string -stream
+            $mapping = _DecodeCertificate($ByteCertificate)
+            $MappingResults.Add( $mapping)
+        }
+        # Digital ID
+        if ($result.Path -like $global:DigitalIDDN ) {
+            [byte[]]$ByteCertificate = $Result.Properties.'usercertificate;binary' | out-string -stream
+            $mapping = _DecodeCertificate($ByteCertificate)
+            $MappingResults.Add( $mapping)
+        }
+
     }
 
-    #Export certificate to byte array
-    [byte[]]$ByteCertificate = $SearchResults[0].Properties.'usercertificate;binary' | out-string -stream
-
-    #Create certificate object from byte array
-    $Certificate = [System.Security.Cryptography.X509Certificates.X509Certificate2] [byte[]]$ByteCertificate
-
-    #temp variables
-    $CertificateSerialArray = $Certificate.SerialNumber.ToCharArray()
-
-    #Reverse certificate serial:
-    for ($i = 0; $i -le $CertificateSerialArray.Length; $i = $i + 2 ) {
-        $reversed_serial = $CertificateSerialArray[$i] + $CertificateSerialArray[$i + 1] + $reversed_serial 
-    }
-
-    #Reverse certificate issuer:
-    $CertificateIssuerArray = $Certificate.Issuer.Split(",")
-
-    foreach ($dn in $CertificateIssuerArray) {
-        $reversedCertificateIssuer = $dn + "," + $reversedCertificateIssuer
-    }
-
-    #Remove empty spaces and trailing coma
-    $reversedCertificateIssuer = $reversedCertificateIssuer.replace(', ', ',')
-    $reversedCertificateIssuer = $reversedCertificateIssuer.Substring(0, $reversedCertificateIssuer.Length - 1)
-
-    #Strong Name Mapping:
-    $mapping = "X509:<I>" + $reversedCertificateIssuer.Trim() + "<SR>" + $reversed_serial
-    return $mapping
+    return $MappingResults
+   
 }
 
 function Get-ADUserEstonianIDMapping {
@@ -71,7 +69,7 @@ function Get-ADUserEstonianIDMapping {
         [Parameter(Mandatory = $true)] [String] $Identity
     )
 
-    #check if activedirectory module exist!
+    # check if activedirectory module exist!
     try {
         Import-Module -name ActiveDirectory
     }
@@ -79,10 +77,12 @@ function Get-ADUserEstonianIDMapping {
         Write-Host "ActiveDirectory Module does not exist"
     }
 
-    #Get AD User mapping
+    # Get AD User mapping
     $userProperties = Get-ADUser -Identity $Identity -Properties altSecurityIdentities
     Return $userProperties.altSecurityIdentities
 }
+
+
 
 function Set-ADUserEstonianIDMapping {
     [CmdletBinding(DefaultParameterSetName = 'EstonianID')]
@@ -97,7 +97,7 @@ function Set-ADUserEstonianIDMapping {
         [Parameter(Mandatory = $true)] [String] $Identity
     )
 
-    #check if activedirectory module exist!
+    # check if activedirectory module exist!
     try {
         Import-Module -name ActiveDirectory
     }
@@ -105,10 +105,10 @@ function Set-ADUserEstonianIDMapping {
         Write-Host "ActiveDirectory Module does not exist"
     }
 
-    #If we pass national id number
+    # If we pass national id number
     $_EstonianID = $EstonianID
    
-    #If we pass active directory nation ID number property   
+    # If we pass active directory nation ID number property   
     if ($EstonianIDPropertyName) {
         Try {
             $userProperties = Get-ADUser -Identity $Identity -Properties $EstonianIDPropertyName
@@ -119,16 +119,22 @@ function Set-ADUserEstonianIDMapping {
         }
     }
 
-    #Get ID certificate mapping
-    $mapping = Get-EstonianIDMapping $_EstonianID
+    # Get ID certificate mapping
+    $mapping = Get-IDUserMapping $_EstonianID
 
-    #Default action is add
+
+    #
+    #   Add action if mapping not found
+    #
+
+    # Default action is add
     if ($Replace) {
-        Set-ADUser -Identity $Identity -replace @{'altsecurityidentities' = "$mapping" }
+        # For replace we clear first
+        Set-ADUser -Identity $Identity -clear altsecurityidentities
     }
-    else {
-        Set-ADUser -Identity $Identity -add @{'altsecurityidentities' = "$mapping" }
-    }
+    foreach ($map in $mapping) {
+        Set-ADUser -Identity $Identity -add @{'altsecurityidentities' = "$map" }
+    }   
 
 }
 
@@ -143,7 +149,7 @@ function Set-ADOUEstonianIDMapping {
         [Parameter()] [int] $Sleep = 0
     )
 
-    #check if activedirectory module exist!
+    # check if activedirectory module exist!
     try {
         Import-Module -name ActiveDirectory
     }
@@ -151,11 +157,10 @@ function Set-ADOUEstonianIDMapping {
         Write-Host "ActiveDirectory does not exist"
     }
 
-    #Collect active users.
-    #$ActiveDirectoryUsers = Get-ADUser -Filter 'enabled -eq $true' -SearchBase $DN -Properties $EstonianIDPropertyName
+    # Collect active users.
     Get-ADUser -Filter 'enabled -eq $true' -SearchBase $DN -Properties $EstonianIDPropertyName | 
     ForEach-Object {
-        #Null user variables
+        # Null user variables
         $_samAccountName = ""
         $_nationalID = ""
         $_userCertificateMapping = ""
@@ -164,36 +169,37 @@ function Set-ADOUEstonianIDMapping {
         $_Can_Be_Replaced = $false
         $_needs_to_be_replaced = $false
         $_replaced = $false
-        #Insert User variables
+        # Insert User variables
         $_samAccountName = $_.SamAccountName
         $_nationalID = $_.$EstonianIDPropertyName     
         $_userCertificateMapping = Get-ADUserEstonianIDMapping -Identity $_samAccountName
 
-        #If altSecuritymapping mapping needs to be replaced
+        # If altSecuritymapping mapping needs to be replaced
         if ($_nationalID) {
-            try{
-                $_nationalIDMapping = Get-EstonianIDMapping $_nationalID
-            }catch{
+            try {
+                $_nationalIDMapping = Get-IDUserMapping $_nationalID
+            }
+            catch {
                 $_nationalIDMapping = $Null 
             }
 
-            #Check if mapping exist
+            # Check if mapping exist
             if ($_userCertificateMapping -eq $_nationalIDMapping) {
                 $_mappingExists = $true
             }
         }
 
-        #If altSecuritymapping can be replaced
+        # If altSecuritymapping can be replaced
         if ($_nationalIDMapping) {
             $_Can_Be_Replaced = $true       
         }
 
-        #If needs to be replaced
+        # If needs to be replaced
         if ($_Can_Be_Replaced -and !$_mappingExists ) {
             $_needs_to_be_replaced = $true    
         }
 
-        #Modify altSecuritymapping that is needed to be replaced
+        # Modify altSecuritymapping that is needed to be replaced
         if ($_needs_to_be_replaced -AND !$WhatIf) {
             if ($Replace) {
                 Set-ADUserEstonianIDMapping -Identity $_samAccountName -EstonianIDPropertyName $EstonianIDPropertyName -Replace
@@ -205,7 +211,7 @@ function Set-ADOUEstonianIDMapping {
         }
 
 
-        #Modify altSecuritymapping that can be replaced
+        # Modify altSecuritymapping that can be replaced
         if ($_Can_Be_Replaced -AND $Force -AND !$WhatIf) {
             if ($Replace) {
                 Set-ADUserEstonianIDMapping -Identity $_samAccountName -EstonianIDPropertyName $EstonianIDPropertyName -Replace
@@ -216,22 +222,20 @@ function Set-ADOUEstonianIDMapping {
             $_replaced = $true
         }
 
-        #Output table object.
+        # Output table object.
         new-object psobject -Property @{
             Account              = $_samAccountName
             NationalID           = $_nationalID
             MappingExists        = $_mappingExists  
-            #            Existing_Mapping     = $_userCertificateMapping
-            #            New_Mapping          = $_nationalIDMapping
             Can_Be_Replaced      = $_Can_Be_Replaced
             Needs_to_be_replaced = $_needs_to_be_replaced
             Replaced             = $_replaced
         } 
 
-        #Format output
+        # Format output
         Format-Table
 
-        #Seprator
+        # Seprator
         Write-Host "-----"
 
         # If sleep is specified then sleep
@@ -239,6 +243,40 @@ function Set-ADOUEstonianIDMapping {
             Start-Sleep -Seconds $Sleep
         }
     }# End for each user
-    #$OutputCollection | Format-Table -AutoSize
 
+}
+
+
+
+#
+# Helper function to decode certificate:
+#
+
+function _DecodeCertificate([byte[]]$CertificateInByte) {
+    
+    # Create certificate object from byte array
+    $Certificate = [System.Security.Cryptography.X509Certificates.X509Certificate2] [byte[]]$CertificateInByte
+
+    # temp variables
+    $CertificateSerialArray = $Certificate.SerialNumber.ToCharArray()
+
+    # Reverse certificate serial:
+    for ($i = 0; $i -le $CertificateSerialArray.Length; $i = $i + 2 ) {
+        $reversed_serial = $CertificateSerialArray[$i] + $CertificateSerialArray[$i + 1] + $reversed_serial 
+    }
+
+    # Reverse certificate issuer:
+    $CertificateIssuerArray = $Certificate.Issuer.Split(",")
+
+    foreach ($dn in $CertificateIssuerArray) {
+        $reversedCertificateIssuer = $dn + "," + $reversedCertificateIssuer
+    }
+
+    # Remove empty spaces and trailing coma
+    $reversedCertificateIssuer = $reversedCertificateIssuer.replace(', ', ',')
+    $reversedCertificateIssuer = $reversedCertificateIssuer.Substring(0, $reversedCertificateIssuer.Length - 1)
+
+    # Strong Name Mapping:
+    $mapping = "X509:<I>" + $reversedCertificateIssuer.Trim() + "<SR>" + $reversed_serial
+    return $mapping
 }
